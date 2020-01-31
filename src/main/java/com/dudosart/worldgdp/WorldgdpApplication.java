8efebbb62a573fca8153ebd3847b5d377d20e0b0
savimbi <kig@lI18>
2020-01-31 12:55:01 +0900

    The begining project to list world bank api

---
+++ src/main/java/com/dudosart/worldgdp/WorldgdpApplication.java
@@ -0,0 +1,13 @@
+package com.dudosart.worldgdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldgdpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldgdpApplication.class, args);
+	}
+
+}
